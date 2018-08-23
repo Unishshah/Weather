d3a2d0ec0bb6dbb2ec0696a5d0e4440fab28ef8d
Unishshah <unishshah@gmail.com>
2018-08-22 22:23:29 -0400

    initial commit

---
+++ src/testing.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class testing extends React.Component
+{
+    render()
+    {
+        return(
+        
+            <div> <h1>Hi i m testing</h1>
+            </div>
+            
+        )
+    }
+}
+
+export default testing;
